1c6ecbf336dbfd819dde40e7462fae7b8c3a56b6
JovaniHM <jovani.hernandez@vlim.com.mx>
2021-02-25 15:44:22 -0600

    Demo de la prueba de la API

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3001;
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.get('/saludo', (req, res) => {
+    res.send('Que onda');
+})
+
+app.post('/productoNotification', (req, res) => {
+    console.log(req.headers);
+    console.log(req.body);
+    /* console.log(req); */
+    res.send('llego el producto');
+})
+
+app.listen(port, () => {
+    console.log(`>>> The server is ready in port: (${port})`);
+    console.log(`>>> Press CTRL + c to finish the server`);
+})
